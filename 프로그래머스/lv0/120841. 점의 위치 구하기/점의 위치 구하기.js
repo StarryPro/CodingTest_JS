32ce5900400979e73134bc94a79a82ac7a2e9541
HanSungUk <hansunguk6178@gmail.com>
2023-03-28 11:04:30 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,10 @@
+function solution(dot) {
+    let answer = 0
+    if(dot[0] > 0){
+        dot[1] > 0 ? answer = 1 : answer = 4
+    }
+    if(dot[0] <0){
+       dot[1] > 0 ? answer = 2 : answer = 3
+       }
+    return answer;
+}
